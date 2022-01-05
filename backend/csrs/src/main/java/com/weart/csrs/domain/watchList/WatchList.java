791f60c56020f27fcb6ccc1d197f74406634e8a4
Hongdonggeon <ghd700@naver.com>
2022-01-05 20:39:45 +0900

    feat(watchlist): watchList(예술품 관심 목록) 엔티티 클래스 추가

---
+++ backend/csrs/src/main/java/com/weart/csrs/domain/watchList/WatchList.java
@@ -0,0 +1,26 @@
+package com.weart.csrs.domain.watchList;
+
+import com.weart.csrs.domain.art.Art;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class WatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "WATCH_LIST_ID")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ART_ID")
+    private Art artId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date addedDate;
+
+    @Column(nullable = false)
+    private Boolean alarmFlag;
+}
