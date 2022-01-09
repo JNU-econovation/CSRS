c5a04a47b9dca300c418476bc6150129396cda78
Hongdonggeon <ghd700@naver.com>
2022-01-09 09:46:10 +0900

    feat(credit): credit(크레딧 거래 내역) 엔티티 클래스 추가

---
+++ backend/csrs/src/main/java/com/weart/csrs/domain/credit/Credit.java
@@ -0,0 +1,18 @@
+package com.weart.csrs.domain.credit;
+
+import com.weart.csrs.domain.successfulbid.SuccessfulBid;
+
+import javax.persistence.*;
+
+@Entity
+public class Credit {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "SUCCESSFUL_BID_ID")
+    private SuccessfulBid successfulBid;
+
+    @Column(nullable = false)
+    private Long balance;
+}
