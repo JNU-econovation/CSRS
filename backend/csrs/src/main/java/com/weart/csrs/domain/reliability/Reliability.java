621bd84e61edbe510f681501451aa62c7d665250
Hongdonggeon <ghd700@naver.com>
2022-01-09 09:39:13 +0900

    feat(reliability): reliabilitiy(신뢰도) 엔티티 클래스 추가

--- backend/csrs/src/main/java/com/weart/csrs/domain/reliability/Reliability.java
+++ backend/csrs/src/main/java/com/weart/csrs/domain/reliability/Reliability.java
@@ -1,2 +1,18 @@
-package com.weart.csrs.domain.reliability;public class Reliability {
+package com.weart.csrs.domain.reliability;
+
+import com.weart.csrs.domain.successfulbid.SuccessfulBid;
+
+import javax.persistence.*;
+
+@Entity
+public class Reliability {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "SUCCESSFUL_BID_ID")
+    private SuccessfulBid successfulBid;
+
+    @Column(nullable = false)
+    private Long warningScore;
 }
