f06864354cce940bfb547c5ea8d20fee74b2e577
Hongdonggeon <ghd700@naver.com>
2022-01-09 09:31:17 +0900

    feat(successfulbid): successfulbid(낙찰 목록) 엔티티 클래스 추가

---
+++ backend/csrs/src/main/java/com/weart/csrs/domain/successfulbid/SuccessfulBid.java
@@ -0,0 +1,22 @@
+package com.weart.csrs.domain.successfulbid;
+
+import com.weart.csrs.domain.bid.Bid;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class SuccessfulBid {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SUCCESSFUL_BID_ID")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "BID_ID")
+    private Bid bid;
+
+    @Column(nullable = false)
+    private Date deadline;
+
+    @Column(nullable = false)
+    private Boolean purchaseFlag;
+}
