ec079dd5ce8febe86007bdd1927ee2935890c82c
Hongdonggeon <ghd700@naver.com>
2022-01-09 09:15:06 +0900

    feat(bid): bid(입찰목록) 엔티티 클래스 추가

---
+++ backend/csrs/src/main/java/com/weart/csrs/domain/bid/Bid.java
@@ -0,0 +1,24 @@
+package com.weart.csrs.domain.bid;
+
+import com.weart.csrs.domain.art.Art;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Bid {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BID_ID")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ART_ID")
+    private Art art;
+
+    @Column(nullable = false)
+    private Long bidPrice;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date bidDate;
+}
