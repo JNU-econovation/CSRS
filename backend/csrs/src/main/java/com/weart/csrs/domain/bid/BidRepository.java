a78ab573895f633133f9dadb1a5732294f02bd17
Hongdonggeon <ghd700@naver.com>
2022-01-17 15:22:20 +0900

    feat : 예술품 입찰 최고가보다 낮은 가격 제시하면 에러 발생하기 - 입찰을 진행하는 예술품 중 가장 높은 입찰 내역 조회하는 쿼리문 생성 - 가장 높은 입찰 가격보다 낮거나 같은 가격을 제시할 경우 예외 발생

--- backend/csrs/src/main/java/com/weart/csrs/domain/bid/BidRepository.java
+++ backend/csrs/src/main/java/com/weart/csrs/domain/bid/BidRepository.java
@@ -1,6 +1,10 @@
 package com.weart.csrs.domain.bid;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
+    @Query("SELECT max(b.bidPrice) FROM Bid b where b.art.id = :artId ")
+    Long findMaxBidPrice(@Param("artId") Long artId);
 }
--- backend/csrs/src/main/java/com/weart/csrs/service/BidService.java
+++ backend/csrs/src/main/java/com/weart/csrs/service/BidService.java
@@ -16,10 +16,15 @@ public class BidService {
     private final BidRepository bidRepository;
     private final ArtRepository artRepository;
     private static final String NOT_FOUND_ART_MESSAGE = "해당 예술품을 찾을 수 없습니다.";
+    private static final String INVALIDATE_BID_PRICE = "입찰 가격은 입찰 최고 가격보다 더 높은 가격만 제시할 수 있습니다.";
 
     @Transactional
     public Long createBid(Long artId, BidCreateRequestDto bidCreateRequestDto) {
         Art art = artRepository.findById(artId).orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_ART_MESSAGE));
+        Long maxBidPrice = bidRepository.findMaxBidPrice(artId);
+        if (maxBidPrice!=null && maxBidPrice >= bidCreateRequestDto.getBidPrice()) {
+            throw new IllegalArgumentException(INVALIDATE_BID_PRICE);
+        }
         Bid bid = bidRepository.save(bidCreateRequestDto.toBid(art, bidCreateRequestDto.getBidPrice()));
         return bid.getId();
     }
