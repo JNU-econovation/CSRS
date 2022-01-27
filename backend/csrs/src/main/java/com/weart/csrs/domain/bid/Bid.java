065e9101462f25a3080dfd2813f426ce7151660a
Hongdonggeon <ghd700@naver.com>
2022-01-27 22:16:41 +0900

    deploy test

--- backend/csrs/src/main/java/com/weart/csrs/domain/bid/Bid.java
+++ backend/csrs/src/main/java/com/weart/csrs/domain/bid/Bid.java
@@ -6,14 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
